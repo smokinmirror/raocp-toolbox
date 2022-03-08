a86eeb957fe39df1b6a00fd6fb77dc5ee3f0e213
Gczmy <657968310@qq.com>
2022-03-08 19:54:01 +0000

    resolve conflicts

--- raocp/core/scenario_tree.py
+++ raocp/core/scenario_tree.py
@@ -29,8 +29,14 @@ class ScenarioTree:
         self.__data = None  # really, any data associated with the nodes of the tree
         self.__update_children()
 
+    def __num_nonleaf_nodes(self):
+        return np.sum(self.__stages < self.num_stages())
+
     def __update_children(self):
-        pass
+        self.__children = []
+        for i in range(self.__num_nonleaf_nodes()):
+            children_of_i = np.where(self.__ancestors == i)
+            self.__children += children_of_i
 
     def num_nodes(self):
         return len(self.__ancestors)
@@ -42,7 +48,7 @@ class ScenarioTree:
         return self.__ancestors[node_idx]
 
     def children_of(self, node_idx):
-        raise NotImplementedError()
+        return self.__children[node_idx]
 
     def stage_of(self, node_idx):
         if node_idx < 0:
@@ -53,13 +59,15 @@ class ScenarioTree:
         return self.__w_idx[node_idx]
 
     def nodes_at_stage(self, stage_idx):
-        raise NotImplementedError()
+        return np.where(self.__stages == stage_idx)[0]
 
     def probability_of_node(self, node_idx):
         return self.__probability[node_idx]
 
     def siblings_of_node(self, node_idx):
-        raise NotImplementedError()
+        if node_idx == 0:
+            return [0]
+        return self.children_of(self.ancestor_of(node_idx))
 
     def conditional_probabilities_of_children(self, node_idx):
         raise NotImplementedError()
@@ -87,14 +95,14 @@ class MarkovChainScenarioTreeFactory:
         """
         num_nonzero_init_distr = len(list(filter(lambda x: (x > 0), self.__initial_distribution)))
         # Initialise `ancestors`
-        ancestors = np.zeros((num_nonzero_init_distr+1, ))
-        ancestors[0] = np.nan  # node 0 does not have an ancestor
+        ancestors = np.zeros((num_nonzero_init_distr+1, ), dtype=int)
+        ancestors[0] = -1  # node 0 does not have an ancestor
         # Initialise `values`
-        values = np.zeros((num_nonzero_init_distr+1, ))
-        values[0] = np.nan
+        values = np.zeros((num_nonzero_init_distr+1, ), dtype=int)
+        values[0] = -1
         values[1:] = np.flatnonzero(self.__initial_distribution)
         # Initialise `stages`
-        stages = np.ones((num_nonzero_init_distr+1, ))
+        stages = np.ones((num_nonzero_init_distr+1, ), dtype=int)
         stages[0] = 0
 
         cursor = 1
@@ -106,17 +114,22 @@ class MarkovChainScenarioTreeFactory:
                 node_id = cursor + i
                 cover = self.__cover(int(values[node_id]))
                 length_cover = len(cover)
-                ancestors = np.concatenate((ancestors, node_id * np.ones((length_cover, ))))
+                ones = np.ones((length_cover, ), dtype=int)
+                ancestors = np.concatenate((ancestors, node_id * ones))
                 nodes_added_at_stage += length_cover
                 values = np.concatenate((values, cover))
             num_nodes_at_stage = nodes_added_at_stage
             cursor = cursor_new
-            stages = np.concatenate((stages, (1 + stage_idx) * np.ones(nodes_added_at_stage, )))
+            ones = np.ones(nodes_added_at_stage, dtype=int)
+            stages = np.concatenate((stages, (1 + stage_idx) * ones))
+
         for stage_idx in range(self.__stopping_time, self.__num_stages):
             ancestors = np.concatenate((ancestors, range(cursor, cursor+num_nodes_at_stage)))
-            values = np.concatenate((values, values[cursor:]))
             cursor += num_nodes_at_stage
-            stages = np.concatenate((stages, (1 + stage_idx) * np.ones(nodes_added_at_stage, )))
+            ones = np.ones((nodes_added_at_stage,), dtype=int )
+            stages = np.concatenate((stages, (1 + stage_idx) * ones))
+            values = np.concatenate((values, values[-num_nodes_at_stage::]))
+
         return ancestors, values, stages
 
     def __make_probability_values(self):
