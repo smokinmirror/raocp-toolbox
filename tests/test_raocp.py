9264a64a5cba05fde2b7741b840d6dbe9622e8f8
Gczmy <657968310@qq.com>
2022-03-21 17:30:45 +0000

    update for RAOCPconfig and RAOCPFactory in raocp/core/problem_spec.py

--- tests/test_raocp.py
+++ tests/test_raocp.py
@@ -23,47 +23,40 @@ class TestRAOCP(unittest.TestCase):
     @staticmethod
     def __construct_raocp_from_markov():
         if TestRAOCP.__raocp_from_markov is None:
-            # data = {
-            #     "cost": {
-            #         "type": "quadratic",
-            #         "Q": np.eye(4),
-            #         "R": np.eye(2)
-            #     },
-            #     "constraints": {
-            #         "type": "polyhedral",
-            #         "x_min": -np.ones(4, ),
-            #         "x_max": np.ones(4, ),
-            #         "u_min": -np.ones(2, ),
-            #         "u_max": np.ones(2, )
-            #     },
-            #     "dynamics": {
-            #         "type": "linear",
-            #         "A": np.random.rand(4, 4),
-            #         "B": np.random.rand(2, 4)
-            #     },
-            #     "risk": {
-            #         "type": "AV@R",
-            #         "alpha": 0.7,
-            #         "E": 1,
-            #         "F": 1,
-            #         "b": None
-            #     }
-            # }
             tree = TestRAOCP.__tree_from_markov
-            tree.set_data_at_node(5, data)
-            x_5 = np.array([[1],
-                           [1]])
-            u_5 = np.array([[1],
-                           [1]])
-            TestRAOCP.__tree_from_markov = \
-                rc.MarkovChainRAOCPFactory(x_5, u_5, tree.get_data_at_node(5), tree.value_at_node(5)).create()
+            x0 = np.array([[1], [1]])
+
+            Aw1 = np.eye(2)
+            Aw2 = np.eye(2)
+            Aw3 = np.eye(2)
+            As = [Aw1, Aw2, Aw3]
+
+            Bw1 = np.eye(2)
+            Bw2 = np.eye(2)
+            Bw3 = np.eye(2)
+            Bs = [Bw1, Bw2, Bw3]
+
+            cost_type = "quadratic"
+            (Q, R, Pf) = (np.eye(2), np.eye(2), np.eye(2))
+
+            (risk_type, alpha) = ("AVAR", 0.5)
+            (E, F, Kone, b) = (np.eye(2),  # p x n matrix
+                               np.eye(2),  # p x r matrix
+                               "Rn+",
+                               np.ones((2, 1)))  # p vector
+
+            TestRAOCP.__raocp_config = rc.RAOCPconfig(scenario_tree=tree) \
+                .with_possible_As_and_Bs(As, Bs) \
+                .with_all_cost_type(cost_type).with_all_Q(Q).with_all_R(R).with_all_Pf(Pf) \
+                .with_all_risk_type(risk_type).with_all_alpha(alpha).with_all_E(E).with_all_F(F).with_all_Kone(
+                Kone).with_all_b(b)
+            TestRAOCP.__raocp_from_markov = rc.RAOCPfactory(problem_config=TestRAOCP.__raocp_config,
+                                                            root_state=x0).create()
 
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
         TestRAOCP.__construct_tree_from_markov()
         TestRAOCP.__construct_raocp_from_markov()
-
-    # def test_cost(self):
 
 
