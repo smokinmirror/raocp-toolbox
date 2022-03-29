8c1c4abc644d6b679701978f3c5eed93735eee14
Gczmy <657968310@qq.com>
2022-03-29 17:38:57 +0100

    modify AVAR's cone

--- raocp/core/risks.py
+++ raocp/core/risks.py
@@ -31,7 +31,7 @@ class AVaR:
         eye = np.eye(self.__num_children)
         self.__E = np.vstack((self.__alpha*eye, -eye, np.ones((1, self.__num_children))))
         self.__b = np.vstack((self.__pi, np.zeros((self.__num_children, 1)), 1))
-        self.__cone = core_cones.NonnegOrth()
+        self.__cone = core_cones.Cart([core_cones.NonnegOrth(), core_cones.NonnegOrth(), core_cones.Zero()])
 
     # GETTERS
     @property
