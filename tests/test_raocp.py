7893d56d745562044877a020b0e31b12cdc0785c
Gczmy <657968310@qq.com>
2022-03-29 18:47:25 +0100

    added test_cost

--- tests/test_raocp.py
+++ tests/test_raocp.py
@@ -125,7 +125,45 @@ class TestRAOCP(unittest.TestCase):
             self.assertEqual(20, cart.dimension)
         for i_cones in range(len(cones_type)):
             self.assertEqual(cones_dimension[i_cones], cart.dimensions[i_cones])
-            
+
+    def test_cost(self):
+        tree = TestRAOCP.__tree_from_markov
+        raocp = TestRAOCP.__raocp_from_markov
+
+        # create test examples
+        x0 = np.array([[1],
+                       [1]])
+
+        def x_all(initial_state):
+            x_list = [initial_state]
+            for i in range(1, tree.num_nodes()):
+                node = tree.ancestor_of(i)
+                x_at_node = raocp.A_at_node(i) @ x_list[node]
+                x_list.append(x_at_node)
+            return x_list
+
+        cost_type = "quadratic"
+        Q = 10 * np.eye(2)  # n x n matrix
+        R = np.eye(2)  # u x u matrix OR scalar
+        Pf = 5 * np.eye(2)  # n x n matrix
+        cost = [10.0, 10.0, 40.0, 10.0, 40.0, 90.0, 40.0, 160.0, 10.0, 40.0, 90.0, 40.0, 160.0, 360.0, 810.0, 40.0,
+                160.0, 360.0, 160.0, 640.0, 10.0, 160.0, 810.0, 40.0, 640.0, 1440.0, 7290.0, 40.0, 640.0, 3240.0, 160.0,
+                2560.0]
+
+        # test cost
+        for i_node in range(tree.num_nodes()):
+            self.assertEqual(cost_type, raocp.cost_item_at_node(i_node).type)
+            self.assertEqual(cost[i_node], raocp.cost_item_at_node(i_node).get_cost(x_all(x0)[i_node]))
+            for row in range(Q.shape[0]):
+                for column in range(Q.shape[1]):
+                    self.assertEqual(Q[row, column], raocp.cost_item_at_node(i_node).Q[row, column])
+            for row in range(R.shape[0]):
+                for column in range(R.shape[1]):
+                    self.assertEqual(R[row, column], raocp.cost_item_at_node(i_node).R[row, column])
+            for row in range(Pf.shape[0]):
+                for column in range(Pf.shape[1]):
+                    self.assertEqual(Pf[row, column], raocp.cost_item_at_node(i_node).Pf[row, column])
+
     def test_risk(self):
         tol = 1e-10
         tree = TestRAOCP.__tree_from_markov
