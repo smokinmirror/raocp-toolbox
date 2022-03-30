85806ccd10dbdece04519e6eb87b80c1b257b708
Ruairi Moran <ruairi@moteam.com>
2022-03-30 18:45:05 +0100

    cache structure change

--- raocp/core/cache.py
+++ raocp/core/cache.py
@@ -11,12 +11,14 @@ class Cache:
     def __init__(self, problem_spec: ps.RAOCP, initial_state):
         self.__raocp = problem_spec
         self.__initial_state = initial_state
+        # primal
         self.__x = [np.empty(0) * self.__raocp.tree.num_nodes()]
         self.__u = [np.empty(0) * self.__raocp.tree.num_nonleaf_nodes()]
-        self.__y = [np.empty(0) * self.__raocp.tree.num_nodes()-1]
+        self.__y = [np.empty(0) * self.__raocp.tree.num_nodes()-1, None]
         self.__s = [None, np.empty(0) * self.__raocp.tree.num_nodes()-1]
         self.__t = [None, np.empty(0) * self.__raocp.tree.num_nodes()-1]
         self.__num_prim_parts = 5
+        # dual
         self.__w1 = []
         self.__w2 = []
         self.__w3 = []
@@ -27,19 +29,33 @@ class Cache:
         self.__w8 = []
         self.__w9 = []
         self.__num_dual_parts = 9
+        # dynamic programming
+        self.__P = [None, np.empty((0, 0)) * self.__raocp.tree.num_nodes()-1]
+        self.__q = [None, np.empty(0) * self.__raocp.tree.num_nodes()-1]
+        self.__K = [np.empty((0, 0)) * self.__raocp.tree.num_nonleaf_nodes()-1]
+        self.__d = [np.empty(0) * self.__raocp.tree.num_nonleaf_nodes()-1]
+        self.__Bhat_chol = [None, np.empty((0, 0)) * self.__raocp.tree.num_nodes()-1]
+        self.__AplusBK = [np.empty((0, 0)) * self.__raocp.tree.num_nonleaf_nodes() - 1]
 
-    # prox_f
+    # OFFLINE ##########################################################################################################
+
+    def offline(self):
+        pass
+
+    # ONLINE ###########################################################################################################
+
+    # prox_f -----------------------------------------------------------------------------------------------------------
 
     def project_on_S1(self):
         """
         z1 = [[x0 ... xN], [u0 ... u(N-1)]]
-        :returns: projection of x, u onto space S_1
+        :returns: nothing
         """
         pass
 
     def project_on_S2(self):
         """
-        z2 = [[y0 ... x(N-1)], [s1 ... sN], [t1 ... tN]]
+        z2 = [[y0 ... y(N-1)], [s1 ... sN], [t1 ... tN]]
         :returns: nothing
         """
         for i in range(self.__raocp.num_nonleaf_nodes):
@@ -49,22 +65,21 @@ class Cache:
             kernel = np.array([[self.__raocp.risk_item_at_node(i).E.T, -eye, -eye],
                                [self.__raocp.risk_item_at_node(i).F.T, zeros, zeros]])
             null = sp.linalg.null_space(kernel)
-            vector = np.vstack((self.__z_prim[i, self.__y],
-                                self.__z_prim[children, self.__s],
-                                self.__z_prim[children, self.__t]))
-            proj_z2 = null @ np.linalg.lstsq(null, vector, rcond=None)[0]
-            self.__z_prim_update[i, self.__y] = np.array(proj_z2[0:2 * children + 1])
-            self.__z_prim_update[children, self.__s] = np.array(proj_z2[2 * children + 1:3 * children + 1])
-            self.__z_prim_update[children, self.__t] = np.array(proj_z2[3 * children + 1:4 * children + 1])
+            # vector = np.vstack((self.__z_prim[i, self.__y],
+            #                     self.__z_prim[children, self.__s],
+            #                     self.__z_prim[children, self.__t]))
+            # proj_z2 = null @ np.linalg.lstsq(null, vector, rcond=None)[0]
+            # self.__z_prim_update[i, self.__y] = np.array(proj_z2[0:2 * children + 1])
+            # self.__z_prim_update[children, self.__s] = np.array(proj_z2[2 * children + 1:3 * children + 1])
+            # self.__z_prim_update[children, self.__t] = np.array(proj_z2[3 * children + 1:4 * children + 1])
 
-    def proximal_f(self, z):
-        self.__z_prim = z
+    def proximal_f(self):
+        # s0 ?
         self.project_on_S1()
         self.project_on_S2()
-        # s0 ?
-        return self.__z_prim_update
+        return "prox_f complete"
 
-    # L / L*
+    # L / L* -----------------------------------------------------------------------------------------------------------
 
     def L(self, z):
         pass
@@ -72,7 +87,7 @@ class Cache:
     def L_adjoint(self):
         pass
 
-    # prox_g*
+    # prox_g* ----------------------------------------------------------------------------------------------------------
 
     def moreau_decomposition(self):
         pass
