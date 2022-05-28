a5f33962d56f85ecae5e7b404a01e7cb8d21388e
Ruairi Moran <ruairi@moteam.com>
2022-05-29 00:19:54 +0100

    cvxpy comparison to dynamic programming is set up but not passing

--- tests/cvxpy_test.py
+++ tests/cvxpy_test.py
@@ -3,35 +3,31 @@ import numpy as np
 
 # Problem data.
 num = 3
-initial_state = np.ones((num, 1))
-A = np.eye(num)
+initial_state = np.ones((num,))
+A = np.eye(num) + np.vstack((np.random.randn(num), np.random.randn(num), np.random.randn(num)))
 B = np.eye(num)
 # x0, x1, x2, x3, u0, u1, u2
-x_and_u_bar = np.array((np.random.randn(num), np.random.randn(num), np.random.randn(num), np.random.randn(num),
-                        np.random.randn(num), np.random.randn(num), np.random.randn(num)))
-print(x_and_u_bar.shape)
+x_bar = np.array((initial_state, np.random.randn(num), np.random.randn(num), np.random.randn(num)))
+u_bar = np.array((np.random.randn(num), np.random.randn(num), np.random.randn(num)))
 
 # Construct the problem.
-x_and_u = cp.Variable(x_and_u_bar.shape)
-objective = cp.Minimize(cp.sum_squares(x_and_u - x_and_u_bar))
-constraints = [x_and_u_bar[0] == initial_state]
-prob = cp.Problem(objective, constraints)
-
-cost = 0
-constr = []
-for t in range(4):
-    cost += cp.sum_squares(x[:,t+1]) + cp.sum_squares(u[:,t])
-    constr += [x[:,t+1] == A@x[:,t] + B@u[:,t],
-               cp.norm(u[:,t], 'inf') <= 1]
+T = x_bar.shape[0] - 1
+x = cp.Variable(x_bar.shape)
+u = cp.Variable(u_bar.shape)
 # sums problem objectives and concatenates constraints.
-constr += [x[:,T] == 0, x[:,0] == x_0]
-problem = cp.Problem(cp.Minimize(cost), constr)
+cost = 0
+constraints = []
+for t in range(T - 1):
+    cost += cp.sum_squares(x[t, :] - x_bar[t, :]) + cp.sum_squares(u[t, :] - u_bar[t, :])
+    constraints += [x[t + 1, :] == A @ x[t, :] + B @ u[t, :]]
+
+constraints += [x[0, :] == initial_state]
+cost += cp.sum_squares(x[T, :] - x_bar[T, :])
+objective = cp.Minimize(cost)
+problem = cp.Problem(objective, constraints)
+
 problem.solve(solver=cp.ECOS)
 
-# # The optimal objective value is returned by `prob.solve()`.
-# result = prob.solve()
-# # The optimal value for x is stored in `x.value`.
-# print(x.value)
-# # The optimal Lagrange multiplier for a constraint is stored in
-# # `constraint.dual_value`.
-# # print(constraints[0].dual_value)
+# The optimal value for x is stored in `x.value`.
+print(f"optimal x arg = {x.value}\n"
+      f"optimal u arg = {u.value}")
