b65395c1be226a1d8adf86ae430da4207d72b6a1
Ruairi Moran <ruairi@moteam.com>
2022-05-28 14:09:05 +0100

    tidying up

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class TestSolver(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, True)  # add assertion here
 
 
 if __name__ == '__main__':
